b644a06362b2d2c3ff0b1e1e9b0891ecf5c27b27
Serg Bogomolov <serg@bogomolov.su>
2022-04-26 19:31:37 +0300

    Hibernate 1

---
+++ Lesson5/lesson5_db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE tovar;
+
+CREATE SCHEMA public AUTHORIZATION postgres;
+COMMENT ON SCHEMA public IS 'standard public schema for tovar database';
+
+GRANT ALL ON SCHEMA public TO PUBLIC;
+GRANT ALL ON SCHEMA public TO postgres;
+
+CREATE TABLE public.tovar (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL) NOT NULL;
+
+CREATE UNIQUE INDEX products_id_uindex ON public.products (id);
+
+INSERT INTO public.tovar (name, price) VALUES
+('Кофе', 200.0),
+('Чай', 100.0),
+('Какао', 150.0),
+('Лимонад', 70.0),
+('Сидор', 125.0);
---
+++ Lesson5/src/main/java/ru/service/ProductService.java
@@ -0,0 +1,17 @@
+package ru.service;
+
+import com.geekbrains.persistence.entities.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProductList();
+
+    void saveOrUpdate(Product product);
+
+    Product getProductById(Long id);
+
+    void deleteById(Long id);
+
+}
