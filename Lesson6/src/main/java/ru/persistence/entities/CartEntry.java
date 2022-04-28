941bd62b9bcb796ea634ad12d1f6d67d038b03ba
Serg Bogomolov <serg@bogomolov.su>
2022-04-28 22:00:18 +0300

    Hibernate 2

---
+++ Lesson6/src/main/java/ru/persistence/entities/CartEntry.java
@@ -0,0 +1,35 @@
+package ru.persistence.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class CartEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private Integer id;
+
+    @OneToMany
+    @JoinColumn (name = "products_id")
+    private Product product;
+
+    @Column (name = "quantity")
+    private Integer quantity;
+
+    @Column (name = "nomer_order")
+    private String nomer_order;
+
+    @Column (name = "data_order")
+    private Data data_order;
+
+    @Override
+    public String toString() {
+        return String.format("\nCartEntry {order_id = %s, product_id = %s, количество = %s, номер = %s, дата = %s}",
+                id, product.getId(), quantity, nomer_order, data_order);
+    }
+}
